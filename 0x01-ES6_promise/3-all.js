d8af4224471515988c37ef3113492aa2d7cb0cc4
johnlivingprooff <johnlivingprooff@gmail.com>
2024-04-24 22:14:43 +0200

    4. Simple promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((user) => {
-      console.log(`${user[0].body} ${user[1].firstName} ${user[1].lastName}`);
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.body.firstName} ${user.body.lastName}`);
     })
     .catch(() => {
       console.error('Signup system offline');
