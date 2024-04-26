ad7c98549f2ad5358067cc9441bed02724d3b4c7
johnlivingprooff <johnlivingprooff@gmail.com>
2024-04-26 06:29:22 +0200

    10. Vroom

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,35 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  cloneCar() {
+    return new Car(this.brand, this.motor, this.color);
+  }
+}
